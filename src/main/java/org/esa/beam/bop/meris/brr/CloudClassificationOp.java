76bc89791887ef53b70371c8afceae869f3354d6
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-09-26 09:39:24 +0000

    made class threadsafe (move dataObjects out of member fields)
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x-contrib/trunk/bop-meris-brr@757 d90d2d48-2b33-0410-a973-e72f85796a02

--- src/main/java/org/esa/beam/bop/meris/brr/CloudClassificationOp.java
+++ src/main/java/org/esa/beam/bop/meris/brr/CloudClassificationOp.java
@@ -66,21 +66,8 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
 
     private DpmConfig dpmConfig;
     private L2AuxData auxData;
-
-    private float[][] rhoToa;
-    private Raster[] radiance;
-    private short[] detectorIndex;
-    private float[] sza;
-    private float[] vza;
-    private float[] saa;
-    private float[] vaa;
-    private float[] altitude;
-    private float[] ecmwfPressure;
-    private FlagWrapper l1Flags;
-
-    private FlagWrapper cloudFlags;
+    
     private RayleighCorrection rayleighCorrection;
-
     
     @SourceProduct(alias="l1b")
     private Product l1bProduct;
@@ -109,8 +96,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
         } catch (Exception e) {
             throw new OperatorException("could not load L2Auxdata", e);
         }
-        rhoToa = new float[EnvisatConstants.MERIS_L1B_NUM_SPECTRAL_BANDS][0];
-        radiance = new Raster[3];
+        
         return createTargetProduct();
     }
 
@@ -139,30 +125,36 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
         return flagCoding;
     }
 
-    private void loadSourceTiles(Rectangle rectangle) throws OperatorException {
+    private SourceData loadSourceTiles(Rectangle rectangle) throws OperatorException {
 
+    	SourceData sd = new SourceData();
+    	sd.rhoToa = new float[EnvisatConstants.MERIS_L1B_NUM_SPECTRAL_BANDS][0];
+    	sd.radiance = new Raster[3];
+    	
         for (int i = 0; i < EnvisatConstants.MERIS_L1B_NUM_SPECTRAL_BANDS; i++) {
-            rhoToa[i] = (float[]) getRaster(rhoToaProduct.getBand(Rad2ReflOp.RHO_TOA_BAND_PREFIX + "_" + (i + 1)), rectangle).getDataBuffer().getElems();
+            sd.rhoToa[i] = (float[]) getRaster(rhoToaProduct.getBand(Rad2ReflOp.RHO_TOA_BAND_PREFIX + "_" + (i + 1)), rectangle).getDataBuffer().getElems();
         }
-        radiance[BAND_BRIGHT_N] = getRaster(
+        sd.radiance[BAND_BRIGHT_N] = getRaster(
 				l1bProduct.getBand(EnvisatConstants.MERIS_L1B_SPECTRAL_BAND_NAMES[auxData.band_bright_n]),
 				rectangle);
-		radiance[BAND_SLOPE_N_1] = getRaster(
+        sd.radiance[BAND_SLOPE_N_1] = getRaster(
 				l1bProduct.getBand(EnvisatConstants.MERIS_L1B_SPECTRAL_BAND_NAMES[auxData.band_slope_n_1]),
 				rectangle);
-		radiance[BAND_SLOPE_N_2] = getRaster(
+        sd.radiance[BAND_SLOPE_N_2] = getRaster(
 				l1bProduct.getBand(EnvisatConstants.MERIS_L1B_SPECTRAL_BAND_NAMES[auxData.band_slope_n_2]),
 				rectangle);
-		detectorIndex = (short[]) getRaster(
+        sd.detectorIndex = (short[]) getRaster(
 				l1bProduct.getBand(EnvisatConstants.MERIS_DETECTOR_INDEX_DS_NAME),
 				rectangle).getDataBuffer().getElems();
-        sza = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_SUN_ZENITH_DS_NAME), rectangle).getDataBuffer().getElems();
-        vza = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_VIEW_ZENITH_DS_NAME), rectangle).getDataBuffer().getElems();
-        saa = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_SUN_AZIMUTH_DS_NAME), rectangle).getDataBuffer().getElems();
-        vaa = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_VIEW_AZIMUTH_DS_NAME), rectangle).getDataBuffer().getElems();
-        altitude = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_DEM_ALTITUDE_DS_NAME), rectangle).getDataBuffer().getElems();
-        ecmwfPressure = (float[]) getRaster(l1bProduct.getTiePointGrid("atm_press"), rectangle).getDataBuffer().getElems();
-        l1Flags = new FlagWrapper.Byte((byte[]) getRaster(l1bProduct.getBand(EnvisatConstants.MERIS_L1B_FLAGS_DS_NAME), rectangle).getDataBuffer().getElems());
+        sd.sza = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_SUN_ZENITH_DS_NAME), rectangle).getDataBuffer().getElems();
+        sd.vza = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_VIEW_ZENITH_DS_NAME), rectangle).getDataBuffer().getElems();
+        sd.saa = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_SUN_AZIMUTH_DS_NAME), rectangle).getDataBuffer().getElems();
+        sd.vaa = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_VIEW_AZIMUTH_DS_NAME), rectangle).getDataBuffer().getElems();
+        sd.altitude = (float[]) getRaster(l1bProduct.getTiePointGrid(EnvisatConstants.MERIS_DEM_ALTITUDE_DS_NAME), rectangle).getDataBuffer().getElems();
+        sd.ecmwfPressure = (float[]) getRaster(l1bProduct.getTiePointGrid("atm_press"), rectangle).getDataBuffer().getElems();
+        sd.l1Flags = new FlagWrapper.Byte((byte[]) getRaster(l1bProduct.getBand(EnvisatConstants.MERIS_L1B_FLAGS_DS_NAME), rectangle).getDataBuffer().getElems());
+        
+        return sd;
     }
 
     @Override
@@ -171,30 +163,30 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
     	Rectangle rectangle = targetRaster.getRectangle();
         pm.beginTask("Processing frame...", rectangle.height + 1);
         try {
-            loadSourceTiles(rectangle);
-            cloudFlags = new FlagWrapper.Short((short[]) targetRaster.getDataBuffer().getElems());
+            SourceData sd = loadSourceTiles(rectangle);
+            FlagWrapper cloudFlags = new FlagWrapper.Short((short[]) targetRaster.getDataBuffer().getElems());
 
             PixelInfo pixelInfo = new PixelInfo();
 			int i = 0;
 			for (int y = rectangle.y; y < rectangle.y + rectangle.height; y++) {
 				pixelInfo.y = y;
 				for (int x = rectangle.x; x < rectangle.x + rectangle.width; x++) {
-					if (!l1Flags.isSet(i, L1_F_INVALID)) {
+					if (!sd.l1Flags.isSet(i, L1_F_INVALID)) {
 						pixelInfo.x = x;
 						pixelInfo.index = i;
 						pixelInfo.airMass = HelperFunctions.calculateAirMass(
-								vza[i], sza[i]);
-						if (l1Flags.isSet(i, L1_F_LAND)) {
+								sd.vza[i], sd.sza[i]);
+						if (sd.l1Flags.isSet(i, L1_F_LAND)) {
 							// ECMWF pressure is only corrected for positive
 							// altitudes and only for land pixels
 							pixelInfo.ecmwfPressure = HelperFunctions
-									.correctEcmwfPressure(ecmwfPressure[i],
-											altitude[i],
+									.correctEcmwfPressure(sd.ecmwfPressure[i],
+											sd.altitude[i],
 											auxData.press_scale_height);
 						} else {
-							pixelInfo.ecmwfPressure = ecmwfPressure[i];
+							pixelInfo.ecmwfPressure = sd.ecmwfPressure[i];
 						}
-						classifyCloud(pixelInfo);
+						classifyCloud(sd, pixelInfo, cloudFlags);
 					}
 					i++;
 				}
@@ -207,16 +199,16 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
         }
     }
 
-    public void classifyCloud(PixelInfo pixelInfo) {
+    public void classifyCloud(SourceData sd, PixelInfo pixelInfo, FlagWrapper cloudFlags) {
         final ReturnValue press = new ReturnValue();
         final boolean[] resultFlags = new boolean[3];
 
         //boolean pcd_poly = Comp_Pressure(pixel) != 0;
-        Comp_Pressure(pixelInfo, press);
+        Comp_Pressure(sd, pixelInfo, press);
         boolean pcd_poly = press.error;
 
         /* apply thresholds on pressure- step 2.1.2 */
-        press_thresh(pixelInfo, press.value, resultFlags);
+        press_thresh(sd, pixelInfo, press.value, resultFlags);
         boolean low_P_nn = resultFlags[0];
         boolean low_P_poly = resultFlags[1];
         boolean delta_p = resultFlags[2];
@@ -229,7 +221,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
         cloudFlags.set(pixelInfo.index, F_CONFIDENCE_P, delta_p);
 
         // Compute slopes- step 2.1.7
-        spec_slopes(pixelInfo, resultFlags);
+        spec_slopes(sd, pixelInfo, resultFlags);
         boolean bright_f = resultFlags[0];
         boolean slope_1_f = resultFlags[1];
         boolean slope_2_f = resultFlags[2];
@@ -239,7 +231,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
 
         // table-driven classification- step 2.1.8
         // DPM #2.1.8-1
-        boolean land_f = l1Flags.isSet(pixelInfo.index, L1_F_LAND);
+        boolean land_f = sd.l1Flags.isSet(pixelInfo.index, L1_F_LAND);
         boolean is_cloud = is_cloudy(land_f,
                                      bright_f,
                                      low_P_nn, low_P_poly, delta_p,
@@ -259,7 +251,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
      * @param pixelInfo the pixel structure
      * @param press the resulting pressure
      */
-    private void Comp_Pressure(PixelInfo pixelInfo, ReturnValue press) {
+    private void Comp_Pressure(SourceData sd, PixelInfo pixelInfo, ReturnValue press) {
         double eta; // Ratio TOAR(11)/TOAR(10)
         press.error = false;
         final FractIndex spectralShiftIndex = new FractIndex();
@@ -269,7 +261,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
 
         /* DPM #2.1.3-1 */
         /* get spectral_shift from detector id in order to use pressure polynomials */
-        Interp.interpCoord(auxData.central_wavelength[bb760][detectorIndex[pixelInfo.index]],
+        Interp.interpCoord(auxData.central_wavelength[bb760][sd.detectorIndex[pixelInfo.index]],
                            auxData.spectral_shift_wavelength,
                            spectralShiftIndex);
 
@@ -278,8 +270,8 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
         // so it works fine with the following.
 
         /* reflectance ratio computation, DPM #2.1.12-2 */
-        if (rhoToa[bb753][pixelInfo.index] > 0.0) {
-            eta = rhoToa[bb760][pixelInfo.index] / rhoToa[bb753][pixelInfo.index];
+        if (sd.rhoToa[bb753][pixelInfo.index] > 0.0) {
+            eta = sd.rhoToa[bb760][pixelInfo.index] / sd.rhoToa[bb753][pixelInfo.index];
         } else {
             // DPM section 3.5.3
             eta = 0.0;        /* DPM #2.1.12-3 */
@@ -287,7 +279,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
         }
         // DPM #2.1.12-4
         Interp.interpCoord(pixelInfo.airMass, auxData.C.getTab(1), cIndex[0]);
-        Interp.interpCoord(rhoToa[bb753][pixelInfo.index], auxData.C.getTab(2), cIndex[1]);
+        Interp.interpCoord(sd.rhoToa[bb753][pixelInfo.index], auxData.C.getTab(2), cIndex[1]);
 
         float[][][] c_lut = (float[][][]) auxData.C.getJavaArray();
         // coefficient used in the pressure estimation
@@ -377,18 +369,18 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
      *                     <code>resultFlags[1]</code> contains low polynomial pressure flag (low_P_poly),
      *                     <code>resultFlags[2]</code> contains pressure range flag (delta_p).
      */
-    private void press_thresh(PixelInfo pixelInfo, double pressure, boolean[] result_flags) {
+    private void press_thresh(SourceData sd, PixelInfo pixelInfo, double pressure, boolean[] result_flags) {
         double delta_press_thresh; /* absolute threshold on pressure difference */
         FractIndex[] DP_Index = FractIndex.createArray(2);
 
         /* get proper threshold - DPM #2.1.2-2 */
-        if (l1Flags.isSet(pixelInfo.index, L1_F_LAND)) {
-            Interp.interpCoord(sza[pixelInfo.index], auxData.DPthresh_land.getTab(0), DP_Index[0]);
-            Interp.interpCoord(vza[pixelInfo.index], auxData.DPthresh_land.getTab(1), DP_Index[1]);
+        if (sd.l1Flags.isSet(pixelInfo.index, L1_F_LAND)) {
+            Interp.interpCoord(sd.sza[pixelInfo.index], auxData.DPthresh_land.getTab(0), DP_Index[0]);
+            Interp.interpCoord(sd.vza[pixelInfo.index], auxData.DPthresh_land.getTab(1), DP_Index[1]);
             delta_press_thresh = Interp.interpolate(auxData.DPthresh_land.getJavaArray(), DP_Index);
         } else {
-            Interp.interpCoord(sza[pixelInfo.index], auxData.DPthresh_ocean.getTab(0), DP_Index[0]);
-            Interp.interpCoord(vza[pixelInfo.index], auxData.DPthresh_ocean.getTab(1), DP_Index[1]);
+            Interp.interpCoord(sd.sza[pixelInfo.index], auxData.DPthresh_ocean.getTab(0), DP_Index[0]);
+            Interp.interpCoord(sd.vza[pixelInfo.index], auxData.DPthresh_ocean.getTab(1), DP_Index[1]);
             delta_press_thresh = Interp.interpolate(auxData.DPthresh_ocean.getJavaArray(), DP_Index);
         }
 
@@ -408,7 +400,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
      *                     <code>resultFlags[1]</code> contains low polynomial pressure flag (low_P_poly),
      *                     <code>resultFlags[2]</code> contains pressure range flag (delta_p).
      */
-    private void spec_slopes(PixelInfo pixelInfo, boolean[] result_flags) {
+    private void spec_slopes(SourceData dc, PixelInfo pixelInfo, boolean[] result_flags) {
         double rhorc_442_thr;   /* threshold on rayleigh corrected reflectance */
         double slope1, slope2;
 
@@ -421,11 +413,11 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
         //Rayleigh correction
         final double[] rhoRay = new double[L1_BAND_NUM];
 
-        double sins = Math.sin(sza[pixelInfo.index] * MathUtils.DTOR);
-        double sinv = Math.sin(vza[pixelInfo.index] * MathUtils.DTOR);
-        double mus = Math.cos(sza[pixelInfo.index] * MathUtils.DTOR);
-        double muv = Math.cos(vza[pixelInfo.index] * MathUtils.DTOR);
-        final double deltaAzimuth = HelperFunctions.computeAzimuthDifference(vaa[pixelInfo.index], saa[pixelInfo.index]);
+        double sins = Math.sin(dc.sza[pixelInfo.index] * MathUtils.DTOR);
+        double sinv = Math.sin(dc.vza[pixelInfo.index] * MathUtils.DTOR);
+        double mus = Math.cos(dc.sza[pixelInfo.index] * MathUtils.DTOR);
+        double muv = Math.cos(dc.vza[pixelInfo.index] * MathUtils.DTOR);
+        final double deltaAzimuth = HelperFunctions.computeAzimuthDifference(dc.vaa[pixelInfo.index], dc.saa[pixelInfo.index]);
 
         /* Rayleigh phase function Fourier decomposition */
         rayleighCorrection.phase_rayleigh(mus, muv, sins, sinv, phaseR);
@@ -436,21 +428,21 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
         rayleighCorrection.tau_rayleigh(press, tauR); /* DPM #2.1.7-2 */
 
         /* Rayleigh reflectance - DPM #2.1.7-3 - v1.3 */
-        rayleighCorrection.ref_rayleigh(deltaAzimuth, sza[pixelInfo.index], vza[pixelInfo.index],
+        rayleighCorrection.ref_rayleigh(deltaAzimuth, dc.sza[pixelInfo.index], dc.vza[pixelInfo.index],
                                         mus, muv, pixelInfo.airMass, phaseR, tauR, rhoRay);
 
         /* DPM #2.1.7-4 */
         for (int band = bb412; band <= bb900; band++) {
-            rhoAg[band] = rhoToa[band][pixelInfo.index] - rhoRay[band];
+            rhoAg[band] = dc.rhoToa[band][pixelInfo.index] - rhoRay[band];
         }
 
         final FractIndex[] rhoRC442index = FractIndex.createArray(3);
         /* Interpolate threshold on rayleigh corrected reflectance - DPM #2.1.7-9 */
-        if (l1Flags.isSet(pixelInfo.index, L1_F_LAND)) {   /* land pixel */
-            Interp.interpCoord(sza[pixelInfo.index],
+        if (dc.l1Flags.isSet(pixelInfo.index, L1_F_LAND)) {   /* land pixel */
+            Interp.interpCoord(dc.sza[pixelInfo.index],
                                auxData.Rhorc_442_land_LUT.getTab(0),
                                rhoRC442index[0]);
-            Interp.interpCoord(vza[pixelInfo.index],
+            Interp.interpCoord(dc.vza[pixelInfo.index],
                                auxData.Rhorc_442_land_LUT.getTab(1),
                                rhoRC442index[1]);
             Interp.interpCoord(deltaAzimuth,
@@ -458,10 +450,10 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
                                rhoRC442index[2]);
             rhorc_442_thr = Interp.interpolate(auxData.Rhorc_442_land_LUT.getJavaArray(), rhoRC442index);
         } else {    /* water  pixel */
-            Interp.interpCoord(sza[pixelInfo.index],
+            Interp.interpCoord(dc.sza[pixelInfo.index],
                                auxData.Rhorc_442_ocean_LUT.getTab(0),
                                rhoRC442index[0]);
-            Interp.interpCoord(vza[pixelInfo.index],
+            Interp.interpCoord(dc.vza[pixelInfo.index],
                                auxData.Rhorc_442_ocean_LUT.getTab(1),
                                rhoRC442index[1]);
             Interp.interpCoord(deltaAzimuth,
@@ -473,7 +465,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
 
         boolean bright_f, slope1_f, slope2_f;
         bright_f = (rhoAg[auxData.band_bright_n] >= rhorc_442_thr)
-                || isSaturated(pixelInfo.x, pixelInfo.y, BAND_BRIGHT_N, auxData.band_bright_n);
+                || isSaturated(dc, pixelInfo.x, pixelInfo.y, BAND_BRIGHT_N, auxData.band_bright_n);
 
         /* Spectral slope processor.brr 1 */
         if (rhoAg[auxData.band_slope_d_1] <= 0.0) {
@@ -483,7 +475,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
             /* DPM #2.1.7-5 */
             slope1 = rhoAg[auxData.band_slope_n_1] / rhoAg[auxData.band_slope_d_1];
             slope1_f = ((slope1 >= auxData.slope_1_low_thr) && (slope1 <= auxData.slope_1_high_thr))
-                    || isSaturated(pixelInfo.x, pixelInfo.y, BAND_SLOPE_N_1, auxData.band_slope_n_1);
+                    || isSaturated(dc, pixelInfo.x, pixelInfo.y, BAND_SLOPE_N_1, auxData.band_slope_n_1);
         }
 
         /* Spectral slope processor.brr 2 */
@@ -494,7 +486,7 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
             /* DPM #2.1.7-7 */
             slope2 = rhoAg[auxData.band_slope_n_2] / rhoAg[auxData.band_slope_d_2];
             slope2_f = ((slope2 >= auxData.slope_2_low_thr) && (slope2 <= auxData.slope_2_high_thr))
-                    || isSaturated(pixelInfo.x, pixelInfo.y, BAND_SLOPE_N_2, auxData.band_slope_n_2);
+                    || isSaturated(dc, pixelInfo.x, pixelInfo.y, BAND_SLOPE_N_2, auxData.band_slope_n_2);
         }
 
         result_flags[0] = bright_f;
@@ -547,9 +539,22 @@ public class CloudClassificationOp extends MerisBasisOp implements Constants {
         return is_cloud;
     }
 
-    private boolean isSaturated(int x, int y, int radianceBandId, int bandId) {
-        return radiance[radianceBandId].getFloat(x, y) > auxData.Saturation_L[bandId];
+    private boolean isSaturated(SourceData sd, int x, int y, int radianceBandId, int bandId) {
+        return sd.radiance[radianceBandId].getFloat(x, y) > auxData.Saturation_L[bandId];
     }
+    
+    private static class SourceData {
+		private float[][] rhoToa;
+		private Raster[] radiance;
+		private short[] detectorIndex;
+		private float[] sza;
+		private float[] vza;
+		private float[] saa;
+		private float[] vaa;
+		private float[] altitude;
+		private float[] ecmwfPressure;
+		private FlagWrapper l1Flags;
+	}
 
     private static class PixelInfo {
         int index;
